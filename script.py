6ebe97beb09c4586d0f657141af83a31534ec96e
mdhamim1226 <41739275+mdhamim1226@users.noreply.github.com>
2021-03-24 14:45:32 +0600

    Update script.py

--- script.py
+++ script.py
@@ -3,57 +3,28 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
+I can help you to find your Movie Or webseries.
 
-This is Version 2 of Auto Filter Bot
+join Our Channel and Group to use me,
 
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
+Channel: https://t.me/joinchat/_dWcJYMtKAk1Yzdl
+Group: https://t.me/PiratedGroup
 
 For more click <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 
-<i>
-* Add bot to your group with admin rights.
+⭕️<b>First Join Our Channel :</b> <a href='https://t.me/joinchat/_dWcJYMtKAk1Yzdl'>Movies and Webseries</a>
+⭕️<b>Then Join Our Group :</b> <a href='https://t.me/PiratedGroup'>pirated Group</a>
 
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
+Now send send the movie or webseries name that your trying to search in our <a href='https://t.me/PiratedGroup'>group</a>
 
+After that Bot will send you the Link of movie or webseries that your trying to search.
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
+If something your trying to search is not uploaded in our <a href='https://t.me/joinchat/_dWcJYMtKAk1Yzdl'>Channel</a> then Bot will not reply you
 
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
+"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
