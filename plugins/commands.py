ab6fc9265b30de3df474e72fddb97c23f1d26efd
mdhamim1226 <41739275+mdhamim1226@users.noreply.github.com>
2021-03-24 14:30:51 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/joinchat/_dWcJYMtKAk1Yzdl")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/joinchat/_dWcJYMtKAk1Yzdl")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/joinchat/_dWcJYMtKAk1Yzdl")
                     ]
                 ]
             ),
